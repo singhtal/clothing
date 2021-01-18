5c1dfa318b6e37e126c0e46eb9f55e4e4e63b320
Tal <talvindersingh1992@gmail.com>
2021-01-18 19:23:28 +0530

    card layout done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './Components/Homepage/Homepage';
+import Homepage from './pages/Homepage/Homepage';
 
 function App() {
   return (
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../Components/directory/directory.js';
+
+const Homepage = () => {
+    return(
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default Homepage;
